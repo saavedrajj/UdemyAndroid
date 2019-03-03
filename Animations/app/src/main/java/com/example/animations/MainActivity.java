89ce93d8bc89b0fbdba71f4e56e724527b34b63c
Juan Saavedra <saavedrajj@gmail.com>
2019-03-03 23:20:25 +0000

    s!

--- Animations/app/src/main/java/com/example/animations/MainActivity.java
+++ Animations/app/src/main/java/com/example/animations/MainActivity.java
@@ -14,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Info","Imageview tapped");
         ImageView bartImageView = (ImageView) findViewById(R.id.bartImageView);
         ImageView homerImageView = (ImageView) findViewById(R.id.homerImageView);
+
+        /*bartImageView.animate().translationXBy(-1000).setDuration(2000);*/
+        /*bartImageView.animate().rotation(180).alpha(0).setDuration(1000);*/
+        /*bartImageView.animate().scaleX(0.5f).scaleY(0.5f).setDuration(1000);*/
+        /*
         if (bartIsShowing) {
             bartIsShowing = false;
             bartImageView.animate().alpha(0).setDuration(2000);
@@ -23,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             bartImageView.animate().alpha(1).setDuration(2000);
             homerImageView.animate().alpha(0).setDuration(2000);
         }
-
+*/
 
 
     }
@@ -32,5 +37,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView bartImageView = (ImageView) findViewById(R.id.bartImageView);
+        bartImageView.setX(-1000);
+        bartImageView.animate().alpha(1).translationXBy(1000).rotation(3600).setDuration(1000);
     }
 }
